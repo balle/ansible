d8c31a8b26060e0477663d091df942e71bc67221
Bastian Ballmann <basti@espy.codekid.net>
2016-04-26 20:40:52 +0200

    added role for minimal desktop

---
+++ roles/desktop/files/ob-autostart.sh
@@ -0,0 +1,20 @@
+#xcompmgr -c -t-5 -l-5 -r4.2 -o.85 &
+#hsetroot -fill /home/basti/Bilder/whax.jpg&
+xscreensaver &
+xbindkeys &
+#tilda &
+#terminator&
+#qcon&
+#yakuake &
+
+DELAY=.5
+#APPS='wmtop \
+#      wmsm -d sda -m \
+#      wmnd \
+#      wmacpi \
+#      wmBinClock \
+#      wmix \
+#      wmmaiload \
+#      wmwifi -tn -i 3 -l \
+#      wmsystemtray'
+#(for X in $APPS ; do ($X &) ; sleep $DELAY ; done) &
